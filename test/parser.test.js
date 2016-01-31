0bcf6cc6d379375efb79936471ac029c6530ec5c
camshaft <bytheway.cameron@gmail.com>
2016-01-31 16:45:30 -0700

    whitespace improvements

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,7 +2,7 @@ var should = require('should');
 var cases = require('postcss-parser-tests');
 var parse = require('..').parse;
 
-describe('parser', function() {
+describe('postcss parser tests', function() {
   cases.each(function (name, css, ideal) {
     it('parses ' + name, function () {
       var root = parse(css, { from: name });
